10ff92646adc82a42323ea2dca19e3b8252c48a4
Ashwin U Iyer <ashwiniyer1706@gmail.com>
2021-03-20 14:39:46 +0530

    Update folder structure  (#5)
    * Initial commit
    
    * Delete Anusuchak.js
    
    * Delete Anusuchi.js
    
    * Delete Components directory
    
    * Delete CreateNoteComponent.js
    
    * Delete NotesScreenComponent.js
    
    * Update folder structure
    
    * Update README.md

--- App.js
+++ App.js
@@ -1,28 +1,44 @@
-// In App.js in a new project
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Anusuchak from './Anusuchak.js';
-import Anusuchi from './Anusuchi.js';
-import Suchi from './Suchi.js';
-import Menu from './Menu.js';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Anusuchak from "./src/Anusuchak/Anusuchak.js";
+import Anusuchi from "./src/Anusuchi/Anusuchi.js";
+import Suchi from "./src/Suchi/Suchi.js";
+import Menu from "./src/Menu/Menu.js";
 
 //Import suchi, anusuchi and anusuchak like normal components
 const Stack = createStackNavigator();
 function App() {
-	//Replace suchicomponent in the component prop for the screen
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name="Menu" component={Menu} options={{ headerShown: false }} style={{ flex: 1 }} />
-				<Stack.Screen name="Suchi" component={Suchi} options={{ headerShown: false }} />
-				<Stack.Screen name="Anusuchi" component={Anusuchi} options={{ headerShown: false }} />
-				<Stack.Screen name="Anusuchak" component={Anusuchak} options={{ headerShown: false }} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  //Replace suchicomponent in the component prop for the screen
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Menu"
+          component={Menu}
+          options={{ headerShown: false }}
+          style={{ flex: 1 }}
+        />
+        <Stack.Screen
+          name="Suchi"
+          component={Suchi}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Anusuchi"
+          component={Anusuchi}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Anusuchak"
+          component={Anusuchak}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 //To navigate to any of the screens in the Stack do
